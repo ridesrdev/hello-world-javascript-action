96d7b2c105797b36c9f85cb6fe674b740eaa5ea3
Richard <r.desro@videotron.ca>
2024-03-04 17:28:17 -0500

    Modifier main.yml et index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+// import { getInput, setOutput, setFailed } from '@actions/core';
+// import { context } from '@actions/github';
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 try{
     // 'who-to-greet' input defined in action metadafile
     const nameToGreet = core.getInput('who-to-greet');
-    console.log('Hello ${nameToGreet}!');
+    console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log('The event payload: ${payload}');
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
-    core.setFailed(error.message);
+    setFailed(error.message);
 }
